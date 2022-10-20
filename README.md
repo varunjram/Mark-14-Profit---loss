7008063f6dc2627edfa702f54fdc282fd2334705
varunjram <varunram.66@gmail.com>
2022-10-20 15:28:59 +0530

    fix

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# Mark-14 Profit & loss -https://brave-turing-3398.netlify.app/
+# Mark-14 Profit & loss
+
+## Netlify - https://brave-turing-3398.netlify.app/
 
 Calculator for Traders for profit/loss calculations
 
 ![image](https://user-images.githubusercontent.com/70443560/196000144-3d0955b7-b588-4f10-8ebb-b5bc15e4cef2.png)
 
- 
+Features :
+
+1. You can calculate your profit/loss in total value and Percentage.
+
+Usage:
+You need to enter the Initial value, Quantity of stocks bought, and the current value in the respective fields and click on the Tell me button to show the results.
--- app.js
+++ app.js
@@ -1,51 +1,43 @@
 const initialPrice = document.querySelector("#initial-price");
 const stocksQuantity = document.querySelector("#stocks-quantity");
-const currentPrice = document.querySelector("#current-price")
-
+const currentPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#calculate-profit");
-
 const outputBox = document.querySelector("#output-box");
+const form = document.querySelector("#form");
+const grey = document.querySelector(".grey");
+submitBtn.addEventListener("click", submitHandler);
+form.addEventListener("submit", (e) => e.preventDefault());
 
-submitBtn.addEventListener('click',submitHandler)
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var curr = Number(currentPrice.value);
 
-function submitHandler () {
-
-    var ip = Number(initialPrice.value) ;
-    var qty = Number(stocksQuantity.value) ;
-    var curr = Number(currentPrice.value) ;
-
-     calculateProfitAndLoss(ip,qty,curr);
-
+  calculateProfitAndLoss(ip, qty, curr);
 }
 
-
-
-function calculateProfitAndLoss(initial,quantity,current) {
-    if (initial > current) {
-        // loss logic here
-        var loss = (initial - current) * quantity
-        var lossPercent = (loss/(initial*quantity))*100 
-
-        // outputBox.innerText ='sorry for your loss of ' + loss + '₹ and your loss-percentage is ' + lossPercent;
-
-        outputBox.innerText =`Sorry for your loss of ${loss} ₹ and your loss-percentage is ${lossPercent}%`;
-
-
-    } else if (current> initial) {
-        // profit logic here
-        var profit = (current-initial)*quantity
-        // var profitPercentage = (profit/(initial*quantity))*100
-        var profitPercentage = ((current-initial)/initial)*100
-
-        outputBox.innerText =`Congratulations on your profit of ${profit}₹₹₹ and your profit-percentage is ${profitPercentage}%`
-
-
-    } else {
-
-        outputBox.innerText = ('No Pain no gain, No gain No pain');
-
-    }
-
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current && initial > 0) {
+    // loss logic here
+    var loss = (initial - current) * quantity;
+    var lossPercent = (loss / (initial * quantity)).toFixed(2) * 100;
+    // outputBox.innerText ='sorry for your loss of ' + loss + '₹ and your loss-percentage is ' + lossPercent;
+    outputBox.innerText = `Sorry for your loss of ${loss} ₹ and your loss-percentage is ${lossPercent}%`;
+    grey.style.backgroundColor = "red";
+    grey.style.transition = "all 0.2s ease-in";
+  } else if (current > initial && initial > 0) {
+    // profit logic here
+    var profit = (current - initial) * quantity;
+    // var profitPercentage = (profit/(initial*quantity))*100
+    var profitPercentage = ((current - initial) / initial).toFixed(2) * 100;
+    outputBox.innerText = `Congratulations on your profit of ${profit}₹₹₹ and your profit-percentage is ${profitPercentage}%`;
+    grey.style.backgroundColor = "green";
+    grey.style.transition = "all 0.2s ease-in";
+  } else if (initial == current && initial > 0) {
+    outputBox.innerText = "No Pain no gain, No gain No pain";
+    grey.style.backgroundColor = "lightyellow";
+    grey.style.transition = "all 0.2s ease-in";
+    grey.style.color = "black";
+  }
 }
-// 
-
+//
